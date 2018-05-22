4fb929603bdff6ffc3e12cef7b8cd0d1c9dec819
renegng <renegng@gmail.com>
2018-05-22 14:12:58 -0600

    Added webpack, babel and sass libraries functionality through npm

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# swing_cms
-A Flask (Python) based CMS that allows people to Swing easily over Web site creation.
+# Swing CMS
+A Flask (Python) based CMS that allows people to Swing easily over WEBsite development.
 
 -RXDBit
+
+# Requirements
+
+Swing CMS is being develop with the following libraries:
+- Python 3.5.2 (on a virtual environment)
+- Python pip 10.0.1
+- Flask 1.0.2
+- Apache HTTPD 2.4.18
+- libapache2-mod-wsgi-py3
+- NodeJS 10.1.0
+- npm 6.0.1
+- npm libraries:
+    - webpack@3 
+    - css-loader
+    - sass-loader
+    - node-sass
+    - extract-loader
+    - file-loader
+    - babel-core 
+    - babel-loader
+    - babel-preset-es2015
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+module.exports = [{
+    entry: './static/css/swing_app.scss',
+    output: {
+        path: path.resolve(__dirname, "static/css"),
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
+        filename: 'style-bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'swing-bundle.css',
+                    },
+                },
+                { loader: 'extract-loader' },
+                { loader: 'css-loader' },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        includePaths: ['./node_modules']
+                    }
+                },
+            ]
+        }]
+    },
+},
+{
+    entry: "./static/js/swing_app.js",
+    output: {
+        path: path.resolve(__dirname, "static/js"),
+        filename: "swing-bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
+}];
